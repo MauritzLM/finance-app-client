c468ae0f2c83c1e1f9c434dd50dc53e45b1d293b
Mauritz Marais <mauritz303@gmail.com>
2024-11-19 15:37:07 +0200

    added user prop

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom"
+import { userObj } from "../types"
+
+interface navProps {
+    user: userObj
+}
 
 
-function Navbar() {
+function Navbar({user}: navProps) {
    
     return (
         <>
         <nav>
+            {user.user.username && <p>Hello {user.user.username}</p>}
             <ul>
                 <li><Link to="/">Overview</Link></li>
                 <li><Link to="/transactions">Transactions</Link></li>
