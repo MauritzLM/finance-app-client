88be6a3fadc746959453097aa1a2295656313030
Mauritz Marais <mauritz303@gmail.com>
2024-11-20 14:58:22 +0200

    added types for pot, transaction, budget and overview data

--- src/types.ts
+++ src/types.ts
@@ -4,12 +4,46 @@ export interface loginFormData {
 }
 
 export interface signupFormData {
-    name: string,
+    username: string,
     email: string,
     password: string
 }
 
 export interface userObj {
-    user: {'id': number, 'username': string},
+    user: { 'id': number, 'username': string },
     token: string
+}
+
+export interface budget {
+    id: number,
+    category: string,
+    maximum: number,
+    theme: string
+}
+
+export interface transaction {
+    amount: number,
+    avatar: string,
+    category: string,
+    date: string,
+    id: number,
+    name: string,
+    recurring: boolean
+}
+
+export interface pot {
+    id: number,
+    name: string,
+    target: number,
+    theme: string,
+    total: number
+}
+
+export interface overviewData {
+    budgets: budget[],
+    expenses: transaction[],
+    income: transaction[],
+    pots: pot[],
+    recent_transactions: transaction[],
+    recurring_bills: transaction[]
 }
