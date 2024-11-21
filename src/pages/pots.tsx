a449d6e647f5a906e3b41e894b941c49e821cb3e
Mauritz Marais <mauritz303@gmail.com>
2024-11-21 14:11:16 +0200

    added pot card rendering

--- src/pages/pots.tsx
+++ src/pages/pots.tsx
@@ -1,13 +1,45 @@
-import { userObj } from "../types"
+import { userObj, pot } from "../types"
 
 interface potsProps {
-    user: userObj
+    user: userObj,
+    pots: pot[]
 }
 
-function Pots({ user }: potsProps) {
+function Pots({ user, pots }: potsProps) {
+    // fetch pots if not in state*
     return (
         <>
-            <h1>Pots page</h1>
+            <h1>Pots</h1>  
+            <button>+ Add New Pot</button>
+
+            {/* create pot card for each pot */}
+
+            <div className="pots-container">
+                {pots?.map(pot =>
+                    <div className="pot-card">
+                        <div>
+                            <h2>{pot.name}</h2><button></button>
+                        </div>
+
+                        <div>
+                            <div>
+                                <span>Total Saved</span><span>${pot.total}</span>
+                            </div>
+                            <div className="progress-bar"></div>
+                            <div>
+                                <span>{(pot.total / pot.target) * 100}%</span><span>Target of ${pot.target}</span>
+                            </div>
+                        </div>
+
+                        {/* how to display and populate forms?* */}
+                        <div>
+                            <button>+ Add Money</button>
+                            <button>Withdraw Money</button>
+                        </div>
+                    </div>
+                )}
+
+            </div>
         </>
     )
 }
