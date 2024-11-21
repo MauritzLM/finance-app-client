24e0fa1125491e90d25bea73c64b1e257c11e34a
Mauritz Marais <mauritz303@gmail.com>
2024-11-21 14:12:26 +0200

    added budget summary and budget card rendering

--- src/pages/budgets.tsx
+++ src/pages/budgets.tsx
@@ -1,13 +1,68 @@
-import { userObj } from "../types"
+import { userObj, budget, budget_spending } from "../types"
 
 interface budgetProps {
-    user: userObj
+    user: userObj,
+    budgets: budget[],
+    budgetSpending: budget_spending
 }
-function Budgets({ user }: budgetProps) {
+
+function Budgets({ user, budgets, budgetSpending }: budgetProps) {
+
+    // fetch latest spending*
+
+    // fetch budgets and spending if not in props
 
     return (
         <>
-            <h1>Budgets page</h1>
+            <h1>Budgets</h1>
+            <button>+ Add New Budget</button>
+
+            {/* summary */}
+            {budgets.length > 0 &&
+                <div>
+                    <div className="budgets-summary">
+
+                        <span>{Math.abs(Object.values(budgetSpending).reduce((a, c) => a + c, 0))}</span>
+                        <span>of {budgets.reduce((a, c) => a + c.maximum, 0)} limit</span>
+
+                        <h2>Spending Summary</h2>
+
+                        <ul>
+                            {budgets.map(budget =>
+                                <li>
+                                    <span>{budget.category}</span>
+                                    <span>{Math.abs(budgetSpending[budget.category])} of {budget.maximum}</span>
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+                    {/* list */}
+                    <div className="budgets-detail">
+
+                        {budgets.map(budget =>
+                            <div>
+                                <h2>{budget.category}</h2>
+                                <button></button>
+                                <p>maximum of {budget.maximum}</p>
+                                <div>
+                                    <div>
+                                        <span>Spent</span>
+                                        <span>{Math.abs(budgetSpending[budget.category])}</span>
+                                    </div>
+                                    <div>
+                                        <span>Remaining</span>
+                                        <span>{budget.maximum - Math.abs(budgetSpending[budget.category])}</span>
+                                    </div>
+
+                                </div>
+                                {/* include latest spending* */}
+                            </div>
+                        )
+                        }
+                    </div>
+                </div>
+            }
+
         </>
     )
 }
