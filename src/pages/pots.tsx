0030221286ab8c99a5057bd5003227b73b5ca39f
Mauritz Marais <mauritz303@gmail.com>
2024-11-25 15:43:55 +0200

    added fetch function and pot props

--- src/pages/pots.tsx
+++ src/pages/pots.tsx
@@ -1,12 +1,39 @@
+import { useEffect } from "react"
 import { userObj, pot } from "../types"
 
 interface potsProps {
     user: userObj,
-    pots: pot[]
+    pots: pot[],
+    updatePots: (potArr: pot[]) => void
 }
 
-function Pots({ user, pots }: potsProps) {
-    // fetch pots if not in state*
+function Pots({ user, pots, updatePots }: potsProps) {
+    // fetch pots if not in state
+    async function getPots() {
+         try {
+            const response = await fetch('http://localhost:8000/finance-api/pots', {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json',
+                    Authorization: `token ${user['token']}`
+                },
+            })
+
+            const data = await response.json()
+ 
+            updatePots(data)
+
+         } catch (error) {
+            console.log(error)
+         }    
+    }
+
+    useEffect(() => {
+        if(!pots.length) {
+            getPots()
+        }
+    })
+
     return (
         <>
             <h1>Pots</h1>  
@@ -16,7 +43,7 @@ function Pots({ user, pots }: potsProps) {
 
             <div className="pots-container">
                 {pots?.map(pot =>
-                    <div className="pot-card">
+                    <div key={pot.name} className="pot-card">
                         <div>
                             <h2>{pot.name}</h2><button></button>
                         </div>
