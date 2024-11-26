b9ce4eca0e5fa7cb12d2d6aa73693673f2fde6b4
Mauritz Marais <mauritz303@gmail.com>
2024-11-26 16:16:55 +0200

    added updated summary, added search and sort functionality

--- src/pages/recurring_bills.tsx
+++ src/pages/recurring_bills.tsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { userObj, transaction } from "../types"
+import { isPaid, totalUpcoming, dueSoon, filterAndSortArr } from "../helpers/helpers"
 
 interface recurringBillsProps {
     user: userObj,
@@ -8,7 +9,9 @@ interface recurringBillsProps {
 }
 
 function RecurringBills({ user, recurringBills, updateRecurringBills }: recurringBillsProps) {
-  
+    const [searchTerm, setSearchTerm] = useState<string>('')
+    const [sortBy, setSortBy] = useState<string>('Latest')
+
     // fetch function
     async function getRecurringBills() {
         try {
@@ -50,14 +53,44 @@ function RecurringBills({ user, recurringBills, updateRecurringBills }: recurrin
                         </p>
                     </div>
 
-                     {/* add summary */}
-                    <div className="summary"></div>
+                    {/* summary */}
+                    <div className="summary">
+                        <ul>
+                            <li>
+                                <span>Paid Bills</span>
+                                {/* calculate amount* */}
+                                <span>{recurringBills.filter(isPaid).length} (${Math.abs(recurringBills.filter(isPaid).reduce((a, c) => a + c.amount, 0))})</span>
+                            </li>
+                            <li>
+                                <span>Total Upcoming</span>
+                                {/* calculate amount* */}
+                                <span>{recurringBills.filter(totalUpcoming).length} (${Math.abs(recurringBills.filter(totalUpcoming).reduce((a, c) => a + c.amount, 0))})</span>
+                            </li>
+                            <li>
+                                <span>Due Soon</span>
+                                {/* calculate amount* */}
+                                <span>{recurringBills.filter(dueSoon).length} (${Math.abs(recurringBills.filter(dueSoon).reduce((a, c) => a + c.amount, 0))})</span>
+                            </li>
+                        </ul>
+                    </div>
 
                 </div>
             }
 
             <div>
-                {/* add search / sort* */}
+                {/* search */}
+                <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.currentTarget.value)} />
+
+                {/* sort */}
+                <select name="sort" id="sort" onChange={(e) => setSortBy(e.currentTarget.value)}>
+                    <option value="Latest">Latest</option>
+                    <option value="Oldest">Oldest</option>
+                    <option value="A-to-Z">A to Z</option>
+                    <option value="Z-to-A">Z to A</option>
+                    <option value="Highest">Highest</option>
+                    <option value="Lowest">Lowest</option>
+                </select>
+
                 <table>
                     <thead>
                         <tr>
@@ -67,10 +100,10 @@ function RecurringBills({ user, recurringBills, updateRecurringBills }: recurrin
                         </tr>
                     </thead>
                     <tbody>
-                        {recurringBills.length && recurringBills.map(t =>
+                        {recurringBills.length && filterAndSortArr(recurringBills, searchTerm, sortBy).map(t =>
                             <tr key={t.id}>
                                 <td>{t.name}</td>
-                                <td>{t.date}</td>
+                                <td>Monthly-{new Date(t.date).toLocaleString('en-GB', { 'day': 'numeric' })}</td>
                                 <td>${Math.abs(t.amount)}</td>
                             </tr>
                         )}
