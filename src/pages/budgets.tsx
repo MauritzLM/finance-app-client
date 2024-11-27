54660a089e35a01264059c12ade0e6f40f6f9cda
Mauritz Marais <mauritz303@gmail.com>
2024-11-27 14:04:27 +0200

    added hideform state, newbudgetform component and latestSpending component

--- src/pages/budgets.tsx
+++ src/pages/budgets.tsx
@@ -1,15 +1,20 @@
-import { useEffect } from "react"
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react"
 import { userObj, budget, budget_spending } from "../types"
+import LatestSpending from "../components/budgets/latest_spending"
+import NewBudgetForm from "../components/budgets/new_budget_form"
+import { Link } from "react-router-dom"
 
 interface budgetProps {
     user: userObj,
     budgets: budget[],
     budgetSpending: budget_spending,
     updateBudgets: (budgetsArr: budget[]) => void,
-    updateBudgetSpending: (spendingObj: budget_spending) => void
+    updateBudgetSpending: (spendingObj: budget_spending) => void,
 }
 
 function Budgets({ user, budgets, budgetSpending, updateBudgets, updateBudgetSpending }: budgetProps) {
+    const [showNewForm, setShowNewForm] = useState(false)
 
     // fetch budgets and spending if not in props
     async function getBudgets() {
@@ -33,16 +38,21 @@ function Budgets({ user, budgets, budgetSpending, updateBudgets, updateBudgetSpe
         }
     }
 
+    // hide form
+    function hideNewForm() {
+        setShowNewForm(false)
+    }
+
     useEffect(() => {
-        if(!budgets.length || !budgetSpending.length) {
+        if (!budgets.length || !budgetSpending.length) {
             getBudgets()
         }
-    })
+    }, [])
 
     return (
         <>
             <h1>Budgets</h1>
-            <button>+ Add New Budget</button>
+            <button onClick={() => setShowNewForm(true)}>+ Add New Budget</button>
 
             {/* summary */}
             {budgets.length > 0 &&
@@ -68,25 +78,40 @@ function Budgets({ user, budgets, budgetSpending, updateBudgets, updateBudgetSpe
 
                         {budgets.map(budget =>
                             <div key={budget.category}>
-                                <h2>{budget.category}</h2>
-                                <button></button>
-                                <p>maximum of {budget.maximum}</p>
                                 <div>
+                                    <h2>{budget.category}</h2>
+                                    <button></button>
+                                    <p>maximum of {budget.maximum}</p>
                                     <div>
-                                        <span>Spent</span>
-                                        <span>{Math.abs(budgetSpending[budget.category])}</span>
-                                    </div>
-                                    <div>
-                                        <span>Remaining</span>
-                                        <span>{budget.maximum - Math.abs(budgetSpending[budget.category])}</span>
-                                    </div>
+                                        {/* spending bar* */}
+                                        <div></div>
+                                        <div>
+                                            <span>Spent</span>
+                                            <span>{Math.abs(budgetSpending[budget.category])}</span>
+                                        </div>
+                                        <div>
+                                            <span>Remaining</span>
+                                            <span>{budget.maximum - Math.abs(budgetSpending[budget.category])}</span>
+                                        </div>
 
+                                    </div>
                                 </div>
-                                {/* include latest spending* */}
+
+                                {/* latest spending */}
+                                <div>
+                                    <h3>Latest Spending</h3><Link to="/transactions">See All</Link>
+                                    <LatestSpending user={user} category={budget.category} />
+                                </div>
                             </div>
                         )
                         }
                     </div>
+                </div>
+            }
+            {/* new budget form */}
+            {showNewForm &&
+                <div className="form-modal">
+                    <NewBudgetForm user={user} budgets={budgets} hideNewForm={hideNewForm}/>
                 </div>
             }
 
