3ba166723efde39c949a5a66fbd4bf9128aab29e
Mauritz Marais <mauritz303@gmail.com>
2024-11-27 14:01:44 +0200

    component to fetch and display latest spending of in budget

---
+++ src/components/budgets/latest_spending.tsx
@@ -0,0 +1,61 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react"
+import { userObj, transaction } from "../../types"
+
+interface LatestSpendingProps {
+    user: userObj,
+    category: string,
+}
+
+function LatestSpending({ user, category }: LatestSpendingProps) {
+    const [transactions, setTransactions] = useState<transaction[]>([])
+
+    // function to fetch latest transactions
+    async function getLatestSpending() {
+        try {
+            const response = await fetch(`http://localhost:8000/finance-api/budgets/${category}`, {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json',
+                    Authorization: `token ${user['token']}`
+                },
+            })
+
+            const data: transaction[] = await response.json()
+            console.log(data)
+            setTransactions([...data])
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        if (!transactions.length) {
+            getLatestSpending()
+        }
+    }, [])
+
+    return (
+        <>
+            {transactions.length > 0 &&
+                <ul>
+                    {transactions.map(t =>
+                        <li key={t.date}>
+                            <div>
+                                <span>{t.name}</span>
+                            </div>
+                            <div>
+                                <span>{t.amount}</span>
+                                <span>{new Date(t.date).toLocaleString('en-GB', { 'day': 'numeric', 'month': 'short', 'year': 'numeric' })}</span>
+                            </div>
+
+                        </li>
+                    )}
+                </ul>
+            }
+        </>
+    )
+}
+
+export default LatestSpending
