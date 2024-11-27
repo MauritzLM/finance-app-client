d9f7f7cd49007a087053de4b8313874dc36b79fb
Mauritz Marais <mauritz303@gmail.com>
2024-11-27 14:00:22 +0200

    file to keep track of themes and categories

---
+++ src/data/data.ts
@@ -0,0 +1,38 @@
+
+// latest budget spending initial state
+export const latest_budget_spending_initial = {
+    'General': [],
+    'Enterainment': [],
+    'Bills': [],
+    'Dining Out': [],
+    'Personal Care': [],
+    'Transportation': [],
+    'Education': [],
+    'Groceries': [],
+    'Lifestyle': [],
+    'Shopping': []
+}
+
+// categories list
+export const categories = [
+    'General', 'Enterainment', 'Bills', 'Dining Out', 'Personal Care', 'Transportation', 'Education', 'Groceries', 'Lifestyle', 'Shopping',
+]
+
+// theme data
+export const themeData = {
+    'Green': '',
+    'Yellow': '',
+    'Cyan': '',
+    'Navy': '',
+    'Red': '',
+    'Purple': '',
+    'Turquoise': '',
+    'Brown': '',
+    'Magenta': '',
+    'Blue': '',
+    'Navy-grey': '',
+    'Army-green': '',
+    'Pink': '',
+    'Gold': '',
+    'Orange': ''
+}    
