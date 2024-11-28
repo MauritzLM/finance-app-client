f1e390f424f05983bb545c075ffedb9450380b02
Mauritz Marais <mauritz303@gmail.com>
2024-11-28 14:38:33 +0200

    implemented form to delete a budget

---
+++ src/components/budgets/delete_budget_form.tsx
@@ -0,0 +1,59 @@
+import { budget, userObj } from "../../types"
+
+
+interface deleteFormProps {
+    user: userObj,
+    budget: budget,
+    budgets: budget[],
+    updateBudgets: (budgetArr: budget[]) => void,
+    hideDeleteForm: () => void
+}
+
+function DeleteBudgetForm({ user, budget, budgets, updateBudgets, hideDeleteForm }: deleteFormProps) {
+
+    // handle submit
+    async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        try {
+            event.preventDefault()
+            const response = await fetch(`http://localhost:8000/finance-api/budgets/${budget.id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-type': 'application/json',
+                    Authorization: `token ${user['token']}`
+                }
+            })
+
+            const data = await response.json()
+            console.log(data)
+
+            if (response.status === 200) {
+                const newArr: budget[] = budgets.filter(item => item.id !== budget.id)
+
+                updateBudgets(newArr)
+                hideDeleteForm()
+            }
+
+            // errors / response 400*
+
+
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <button type="button" onClick={hideDeleteForm}>close</button>
+                <h2>Delete '{budget.category}'</h2>
+                <p>Are you sure you want to delete this budget? This action cannot be reversed, and all the data inside it will be removed forever.</p>
+
+                <button type="submit">Yes, Confirm Deletion</button>
+                <button type="button" onClick={hideDeleteForm}>No, Go Back</button>
+            </form>
+        </>
+    )
+}
+
+export default DeleteBudgetForm
