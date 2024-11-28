72342b62e50ac85223a54e43625c4aab66b8524b
Mauritz Marais <mauritz303@gmail.com>
2024-11-28 14:34:40 +0200

    updated theme data object

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,4 +1,4 @@
-
+import { strObj } from "../types"
 // latest budget spending initial state
 export const latest_budget_spending_initial = {
     'General': [],
@@ -14,25 +14,25 @@ export const latest_budget_spending_initial = {
 }
 
 // categories list
-export const categories = [
-    'General', 'Enterainment', 'Bills', 'Dining Out', 'Personal Care', 'Transportation', 'Education', 'Groceries', 'Lifestyle', 'Shopping',
+export const categories: string[] = [
+    'General', 'Entertainment', 'Bills', 'Dining Out', 'Personal Care', 'Transportation', 'Education', 'Groceries', 'Lifestyle', 'Shopping',
 ]
 
 // theme data
-export const themeData = {
-    'Green': '',
-    'Yellow': '',
-    'Cyan': '',
-    'Navy': '',
-    'Red': '',
-    'Purple': '',
-    'Turquoise': '',
-    'Brown': '',
-    'Magenta': '',
-    'Blue': '',
-    'Navy-grey': '',
-    'Army-green': '',
-    'Pink': '',
-    'Gold': '',
-    'Orange': ''
+export const themeData: strObj = {
+    'Green': '#277C78',
+    'Yellow': '#F2CDAC',
+    'Cyan': '#82C9D7',
+    'Navy': '#626070',
+    'Red': '#C94736',
+    'Purple': '#826CB0',
+    'Turquoise': '#597C7C',
+    'Brown': '#93674F',
+    'Magenta': '#934F6F',
+    'Blue': '#3F82B2',
+    'Navy-grey': '#97A0AC',
+    'Army-green': '#7F9161',
+    'Pink': '#AF81BA',
+    'Gold': '#CAB361',
+    'Orange': '#BE6C49'
 }    
