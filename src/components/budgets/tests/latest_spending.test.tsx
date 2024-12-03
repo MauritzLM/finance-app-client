8ee500da222d9129c434f667f98e78be3f83c2be
Mauritz Marais <mauritz303@gmail.com>
2024-12-03 13:21:24 +0200

    added rendering test

---
+++ src/components/budgets/tests/latest_spending.test.tsx
@@ -0,0 +1,46 @@
+import { render, screen } from '@testing-library/react'
+import { describe, it, expect, vi } from 'vitest'
+import { BrowserRouter } from 'react-router-dom'
+import '@testing-library/jest-dom'
+import LatestSpending from '../latest_spending'
+import { userObj } from '../../../types'
+
+
+// mocks
+const fetchMock = vi.fn();
+fetchMock.mockReturnValue(
+    Promise.resolve({
+        json: () => Promise.resolve([{ 'amount': 18.50, 'avatar': '', 'category': 'Education', 'date': '2024-08-01T09:25:11Z', 'id': 10, 'name': 'Daniel', 'recurring': false },
+        { 'amount': 17.00, 'avatar': '', 'category': 'Education', 'date': '2024-08-02T09:25:11Z', 'id': 12, 'name': 'Tom', 'recurring': false },
+        { 'amount': 30.00, 'avatar': '', 'category': 'Education', 'date': '2024-07-30T10:05:42Z', 'id': 11, 'name': 'Peter', 'recurring': false },]),
+    }),
+);
+vi.stubGlobal('fetch', fetchMock);
+const mockUser: userObj = { 'user': { 'id': 1, 'username': 'mo' }, 'token': '1234' }
+
+
+describe('test latest spending component', () => {
+    it('test component rendering', async () => {
+        render(<BrowserRouter><LatestSpending user={mockUser} category={'Education'} /></BrowserRouter>)
+
+        const transaction_list = await screen.findAllByRole('listitem');
+
+        expect(transaction_list).toHaveLength(3)
+        
+        // amount
+        const amount_list = await screen.findAllByTestId('amount')
+
+        expect(amount_list).toHaveLength(3)
+        expect(amount_list[1]).toHaveTextContent('-$17.00')
+        expect(amount_list[2]).toHaveTextContent('-$30.00')
+
+        // date
+        const date_list = await screen.findAllByTestId('date')
+
+        expect(date_list).toHaveLength(3)
+        expect(date_list[0]).toHaveTextContent('1 Aug 2024')
+        expect(date_list[2]).toHaveTextContent('30 Jul 2024')
+    });
+});
+
+
