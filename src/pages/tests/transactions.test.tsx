35c3a7b6123244b6eb42a4ed2a2013157b389904
Mauritz Marais <mauritz303@gmail.com>
2024-12-03 15:08:20 +0200

    added tests for dates/amounts and rendering

---
+++ src/pages/tests/transactions.test.tsx
@@ -0,0 +1,61 @@
+import { render, screen } from '@testing-library/react'
+import { describe, it, expect, vi } from 'vitest'
+import { BrowserRouter } from 'react-router-dom'
+import '@testing-library/jest-dom'
+import Transactions from '../transactions'
+import { userObj } from '../../types'
+import { act } from 'react'
+
+
+// mocks
+const fetchMock = vi.fn();
+fetchMock.mockReturnValue(
+    Promise.resolve({
+        json: () => Promise.resolve({
+            'page_list': [{ 'amount': 18.50, 'avatar': '', 'category': 'Education', 'date': '2024-08-01T09:25:11Z', 'id': 10, 'name': 'Daniel', 'recurring': false },
+            { 'amount': -17.00, 'avatar': '', 'category': 'Bills', 'date': '2024-08-02T09:25:11Z', 'id': 11, 'name': 'Tom', 'recurring': true },
+            { 'amount': 30.00, 'avatar': '', 'category': 'Education', 'date': '2024-07-30T10:05:42Z', 'id': 12, 'name': 'Peter', 'recurring': false },
+            ],
+            'num_pages': 3
+        }),
+    }),
+);
+vi.stubGlobal('fetch', fetchMock);
+const mockUser: userObj = { 'user': { 'id': 1, 'username': 'mo' }, 'token': '1234' }
+
+
+// test transaction list, amount / date formatting, number of page buttons
+describe('test transactions component', () => {
+    it('renders transactions list', async () => {
+        render(<BrowserRouter><Transactions user={mockUser} /></BrowserRouter>)
+
+        const transactions = await screen.findAllByTestId('transaction')
+
+        expect(transactions).toHaveLength(3)
+    });
+
+    it('renders correct page buttons', async () => {
+        act(() => {
+            render(<BrowserRouter><Transactions user={mockUser} /></BrowserRouter>)
+        })
+
+        // how to get after update?
+        const page_buttons = await screen.findAllByTestId('page-btn')
+
+        expect(page_buttons).toHaveLength(3)
+    })
+
+    it('correct date and amount format', async () => {
+        render(<BrowserRouter><Transactions user={mockUser} /></BrowserRouter>)
+
+        const dates = await screen.findAllByTestId('date')
+
+        expect(dates[0]).toHaveTextContent('1 Aug 2024')
+        expect(dates[2]).toHaveTextContent('30 Jul 2024')
+
+        const amounts = await screen.findAllByTestId('amount')
+
+        expect(amounts[0]).toHaveTextContent('$18.50')
+        expect(amounts[1]).toHaveTextContent('-$17.00')
+    });
+});
