880e1afacceb241c4789e28816052d4f23517d71
Mauritz Marais <mauritz303@gmail.com>
2024-12-05 15:36:32 +0200

    added tests for displaying forms, updated textcontent expects

--- src/pages/tests/budgets.test.tsx
+++ src/pages/tests/budgets.test.tsx
@@ -4,6 +4,7 @@ import Budgets from '../budgets'
 import { BrowserRouter } from 'react-router-dom'
 import '@testing-library/jest-dom'
 import { budget, budget_spending, userObj } from '../../types'
+import userEvent from '@testing-library/user-event'
 
 // mocks
 const mockUser: userObj = { 'user': { 'id': 1, 'username': 'mo' }, 'token': '1234' }
@@ -18,22 +19,24 @@ fetchMock.mockReturnValue(
 );
 vi.stubGlobal('fetch', fetchMock);
 
+vi.mock('../../components/budgets/latest_spending')
+
 // test rendering
-describe('test component rendering', () => {
+describe('test budget component', () => {
     it('test budget summary', () => {
         render(<BrowserRouter><Budgets user={mockUser} budgets={mockBudgets} budgetSpending={mock_budget_spending} updateBudgetSpending={vi.fn()} updateBudgets={vi.fn()} /></BrowserRouter>)
 
         // correct total and limit
-        expect(screen.getByTestId('total-spent')).toHaveTextContent('$500')
-        expect(screen.getByTestId('limit')).toHaveTextContent('of $750 limit')
+        expect(screen.getByTestId('total-spent').textContent).toMatch('$500')
+        expect(screen.getByTestId('limit').textContent).toMatch('of $750 limit')
 
         // spending of maximum
         expect(screen.getAllByTestId('summary-list')).toHaveLength(2)
 
         const summary_spending = screen.getAllByTestId('budget-spending')
         expect(summary_spending).toHaveLength(2)
-        expect(summary_spending[0]).toHaveTextContent('$400.00 of $500.00')
-        expect(summary_spending[1]).toHaveTextContent('$100.00 of $250.00')
+        expect(summary_spending[0].textContent).toMatch('$400.00 of $500.00')
+        expect(summary_spending[1].textContent).toMatch('$100.00 of $250.00')
     });
 
 
@@ -44,22 +47,62 @@ describe('test component rendering', () => {
         const detail_categories = screen.getAllByTestId('detail-category')
 
         expect(detail_categories).toHaveLength(2)
-        expect(detail_categories[0]).toHaveTextContent('Bills')
-        expect(detail_categories[1]).toHaveTextContent('Education')
+        expect(detail_categories[0].textContent).toMatch('Bills')
+        expect(detail_categories[1].textContent).toMatch('Education')
 
         // total spent
         const detail_spending = screen.getAllByTestId('detail-spending')
 
         expect(detail_spending).toHaveLength(2)
-        expect(detail_spending[0]).toHaveTextContent('$400.00')
-        expect(detail_spending[1]).toHaveTextContent('$100.00')
+        expect(detail_spending[0].textContent).toMatch('$400.00')
+        expect(detail_spending[1].textContent).toMatch('$100.00')
 
         // remaining
         const detail_remaining = screen.getAllByTestId('detail-remaining')
 
         expect(detail_remaining).toHaveLength(2)
-        expect(detail_remaining[0]).toHaveTextContent('$100.00')
-        expect(detail_remaining[1]).toHaveTextContent('$150.00')
+        expect(detail_remaining[0].textContent).toMatch('$100.00')
+        expect(detail_remaining[1].textContent).toMatch('$150.00')
     })
+
+    // new, edit, delete
+    it('clicking add new button displays new budget form', async () => {
+        render(<BrowserRouter><Budgets user={mockUser} budgets={mockBudgets} budgetSpending={mock_budget_spending} updateBudgetSpending={vi.fn()} updateBudgets={vi.fn()} /></BrowserRouter>)
+
+        const user = userEvent.setup()
+        const new_btn = screen.getByTestId('new-btn')
+
+        await user.click(new_btn)
+
+        const new_form = await screen.findByTestId('new-form')
+
+        expect(new_form).toBeInTheDocument()
+    });
+
+    it('clicking edit button displays edit budget form', async () => {
+        render(<BrowserRouter><Budgets user={mockUser} budgets={mockBudgets} budgetSpending={mock_budget_spending} updateBudgetSpending={vi.fn()} updateBudgets={vi.fn()} /></BrowserRouter>)
+
+        const user = userEvent.setup()
+        const edit_btns = screen.getAllByTestId('edit-btn')
+
+        await user.click(edit_btns[0])
+
+        const edit_form = await screen.findByTestId('edit-form')
+
+        expect(edit_form).toBeInTheDocument()
+    });
+
+    it('clicking delete button displays delete budget form', async () => {
+        render(<BrowserRouter><Budgets user={mockUser} budgets={mockBudgets} budgetSpending={mock_budget_spending} updateBudgetSpending={vi.fn()} updateBudgets={vi.fn()} /></BrowserRouter>)
+
+        const user = userEvent.setup()
+        const delete_btns = screen.getAllByTestId('delete-btn')
+
+        await user.click(delete_btns[0])
+
+        const delete_form = await screen.findByTestId('delete-form')
+
+        expect(delete_form).toBeInTheDocument()
+    });
 
 });
