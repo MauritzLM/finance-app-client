b401ac09af9dc1115a78a7d3b25e452eb7b2c00c
Mauritz Marais <mauritz303@gmail.com>
2024-12-09 15:46:55 +0200

    added svgs

--- src/pages/transactions.tsx
+++ src/pages/transactions.tsx
@@ -82,33 +82,47 @@ function Transactions({ user }: transactionsProps) {
                 <div>
                     <form onSubmit={(e) => getTransactionsSearch(e)}>
                         <input type="text" name="search" value={searchTerm} onInput={(e) => setSearchTerm(e.currentTarget.value)}></input>
-                        <button type="submit">search</button>
+                        <button type="submit">
+                            <svg fill="none" height="14" viewBox="0 0 14 14" width="14" xmlns="http://www.w3.org/2000/svg"><path d="m13.3538 13.1462-3.1294-3.1287c.907-1.08894 1.3593-2.48564 1.2628-3.89955-.0966-1.41391-.7345-2.73618-1.78109-3.69173-1.0466-.95555-2.42131-1.470821-3.83815-1.438621-1.41683.032201-2.76671.609391-3.76883 1.611501-1.00211 1.00212-1.579301 2.352-1.611501 3.76883-.0322 1.41684.483071 2.79155 1.438621 3.83817.95556 1.0466 2.27782 1.6845 3.69173 1.781 1.41391.0966 2.81061-.3557 3.89954-1.2627l3.12878 3.1293c.0464.0465.1016.0833.1623.1085.0607.0251.1257.0381.1914.0381s.1308-.013.1915-.0381c.0607-.0252.1158-.062.1623-.1085.0464-.0464.0833-.1016.1084-.1623.0252-.0607.0381-.1257.0381-.1914s-.0129-.1308-.0381-.1915c-.0251-.0607-.062-.1158-.1084-.1623zm-11.85378-6.64621c0-.89002.26392-1.76005.75839-2.50007.49446-.74002 1.19727-1.31679 2.01954-1.65739.82226-.34059 1.72706-.42971 2.59998-.25607.87291.17363 1.67473.60221 2.30407 1.23155s1.0579 1.43116 1.2316 2.30407c.1736.87292.0845 1.77772-.2561 2.59999-.34062.82226-.91739 1.52507-1.65741 2.01953-.74002.4945-1.61005.7584-2.50007.7584-1.19307-.0013-2.33689-.4759-3.18052-1.31949-.84363-.84363-1.31816-1.98745-1.31948-3.18052z" fill="#201f24" /></svg>
+                        </button>
                     </form>
 
                     {/* sort */}
-                    <select name="sort" id="sort" onChange={(e) => setSortBy(e.currentTarget.value)}>
-                        <option value="Latest">Latest</option>
-                        <option value="Oldest">Oldest</option>
-                        <option value="A-to-Z">A to Z</option>
-                        <option value="Z-to-A">Z to A</option>
-                        <option value="Highest">Highest</option>
-                        <option value="Lowest">Lowest</option>
-                    </select>
+                    <div>
+                        <button>
+                            <svg fill="none" height="15" viewBox="0 0 16 15" width="16" xmlns="http://www.w3.org/2000/svg"><path d="m14.25 0h-12.5c-.33152 0-.64946.131696-.883884.366116-.23442.234421-.366116.552363-.366116.883884v12.5c0 .3315.131696.6495.366116.8839.234424.2344.552364.3661.883884.3661h12.5c.3315 0 .6495-.1317.8839-.3661s.3661-.5524.3661-.8839v-12.5c0-.331521-.1317-.649463-.3661-.883884-.2344-.23442-.5524-.366116-.8839-.366116zm-10.625 3.125h7.5c.1658 0 .3247.06585.4419.18306.1173.11721.1831.27618.1831.44194s-.0658.32473-.1831.44194c-.1172.11721-.2761.18306-.4419.18306h-7.5c-.16576 0-.32473-.06585-.44194-.18306s-.18306-.27618-.18306-.44194.06585-.32473.18306-.44194.27618-.18306.44194-.18306zm3.125 8.75h-3.125c-.16576 0-.32473-.0658-.44194-.1831-.11721-.1172-.18306-.2761-.18306-.4419s.06585-.3247.18306-.4419c.11721-.1173.27618-.1831.44194-.1831h3.125c.16576 0 .32473.0658.44194.1831.11721.1172.18306.2761.18306.4419s-.06585.3247-.18306.4419c-.11721.1173-.27618.1831-.44194.1831zm.625-3.75h-3.75c-.16576 0-.32473-.06585-.44194-.18306s-.18306-.27618-.18306-.44194.06585-.32473.18306-.44194.27618-.18306.44194-.18306h3.75c.16576 0 .32473.06585.44194.18306s.18306.27618.18306.44194-.06585.32473-.18306.44194-.27618.18306-.44194.18306zm6.0672 2.3172-1.875 1.875c-.0581.0581-.127.1042-.2029.1357-.0758.0314-.1572.0476-.2393.0476s-.1635-.0162-.2393-.0476c-.0759-.0315-.1448-.0776-.2029-.1357l-1.87499-1.875c-.11727-.1173-.18316-.2763-.18316-.4422 0-.16585.06589-.32491.18316-.44219.11728-.11727.27634-.18316.44219-.18316s.32491.06589.44219.18316l.80781.80859v-3.4914c0-.16576.0658-.32473.1831-.44194.1172-.11721.2761-.18306.4419-.18306s.3247.06585.4419.18306c.1173.11721.1831.27618.1831.44194v3.4914l.8078-.80859c.1173-.11727.2763-.18316.4422-.18316s.3249.06589.4422.18316c.1173.11728.1831.27634.1831.44219 0 .1659-.0658.3249-.1831.4422z" fill="#201f24" /></svg>
+                        </button>
+                        <select name="sort" id="sort" onChange={(e) => setSortBy(e.currentTarget.value)}>
+                            <option value="Latest">Latest</option>
+                            <option value="Oldest">Oldest</option>
+                            <option value="A-to-Z">A to Z</option>
+                            <option value="Z-to-A">Z to A</option>
+                            <option value="Highest">Highest</option>
+                            <option value="Lowest">Lowest</option>
+                        </select>
+                        <svg fill="none" height="6" viewBox="0 0 12 6" width="12" xmlns="http://www.w3.org/2000/svg"><path d="m11.3538.85375-5.00002 5c-.04644.04649-.10158.08337-.16228.10853s-.12576.03811-.19147.03811-.13077-.01295-.19147-.03811-.11585-.06204-.16228-.10853l-5.000002-5c-.070006-.069927-.11769-.159054-.137015-.256096-.019325-.097043-.009423-.197638.028453-.289049.037877-.091412.102024-.16953.18432-.224465.082297-.0549354.179044-.08421771.277994-.08413985h9.99997c.099-.00007786.1957.02920445.278.08413985.0823.054935.1465.133053.1843.224465.0379.091411.0478.192006.0285.289049-.0193.097042-.067.186169-.137.256096z" fill="#201f24" /></svg>
+                    </div>
 
                     {/* category */}
-                    <select name="category" id="category" onChange={(e) => setCategory(e.currentTarget.value)}>
-                        <option value="All">All Transactions</option>
-                        <option value="Entertainment">Entertainment</option>
-                        <option value="Bills">Bills</option>
-                        <option value="Groceries">Groceries</option>
-                        <option value="Dining Out">Dining Out</option>
-                        <option value="Transportation">Transportation</option>
-                        <option value="Personal Care">Personal Care</option>
-                        <option value="General">General</option>
-                        <option value="Lifestyle">Lifestyle</option>
-                        <option value="Education">Education</option>
-                        <option value="Shopping">Shopping</option>
-                    </select>
+                    <div>
+                        <button>
+                            <svg fill="none" height="16" viewBox="0 0 18 16" width="18" xmlns="http://www.w3.org/2000/svg"><path d="m16.7976 2.71562-.0062.00704-5.2914 5.65v4.33514c.0003.2062-.0504.4092-.1476.5911-.0972.1818-.2379.3368-.4095.4511l-2.49995 1.6672c-.1884.1255-.40734.1975-.63344.2082-.22611.0108-.45091-.04-.65039-.147s-.36616-.2662-.48225-.4605-.17723-.4165-.17689-.6429v-6.00234l-5.29141-5.65-.00625-.00704c-.16269-.17905-.269938-.40146-.308716-.64026s-.007425-.48373.090256-.70506c.09768-.22133.25749-.409563.46005-.541857.20255-.132294.43914-.202966.68107-.203443h13.75002c.2421.000024.479.070368.6819.202485.2029.132118.3631.320325.4611.541745.0979.22142.1295.46653.0908.70555-.0387.23901-.146.46165-.3088.64084z" fill="#201f24" /></svg>
+                        </button>
+                        <select name="category" id="category" onChange={(e) => setCategory(e.currentTarget.value)}>
+                            <option value="All">All Transactions</option>
+                            <option value="Entertainment">Entertainment</option>
+                            <option value="Bills">Bills</option>
+                            <option value="Groceries">Groceries</option>
+                            <option value="Dining Out">Dining Out</option>
+                            <option value="Transportation">Transportation</option>
+                            <option value="Personal Care">Personal Care</option>
+                            <option value="General">General</option>
+                            <option value="Lifestyle">Lifestyle</option>
+                            <option value="Education">Education</option>
+                            <option value="Shopping">Shopping</option>
+                        </select>
+                        <svg fill="none" height="6" viewBox="0 0 12 6" width="12" xmlns="http://www.w3.org/2000/svg"><path d="m11.3538.85375-5.00002 5c-.04644.04649-.10158.08337-.16228.10853s-.12576.03811-.19147.03811-.13077-.01295-.19147-.03811-.11585-.06204-.16228-.10853l-5.000002-5c-.070006-.069927-.11769-.159054-.137015-.256096-.019325-.097043-.009423-.197638.028453-.289049.037877-.091412.102024-.16953.18432-.224465.082297-.0549354.179044-.08421771.277994-.08413985h9.99997c.099-.00007786.1957.02920445.278.08413985.0823.054935.1465.133053.1843.224465.0379.091411.0478.192006.0285.289049-.0193.097042-.067.186169-.137.256096z" fill="#201f24" /></svg>
+                    </div>
                 </div>
 
                 <table>
@@ -123,7 +137,11 @@ function Transactions({ user }: transactionsProps) {
                     <tbody>
                         {transactions?.map(t =>
                             <tr data-testid="transaction" key={t.id}>
-                                <td>{t.name}</td>
+
+                                <td>
+                                    {/* add image* */}
+                                    {t.name}
+                                </td>
                                 <td>{t.category}</td>
                                 <td data-testid="date">{new Date(t.date).toLocaleString('en-GB', { 'day': 'numeric', 'month': 'short', 'year': 'numeric' })}</td>
                                 <td data-testid="amount" className={t.amount < 0 ? '' : 'income'}>${formatTransaction(t.amount.toString())}</td>
@@ -132,7 +150,10 @@ function Transactions({ user }: transactionsProps) {
                     </tbody>
                 </table>
             </div>
-            <button disabled={pageNumber === 1 ? true : false} onClick={() => setPageNumber(pageNumber - 1)}>Prev</button>
+            <button disabled={pageNumber === 1 ? true : false} onClick={() => setPageNumber(pageNumber - 1)}>
+                <svg fill="none" height="11" viewBox="0 0 6 11" width="6" xmlns="http://www.w3.org/2000/svg"><path d="m5.14656 10.8535-5.000005-4.99997c-.046488-.04643-.0833676-.10158-.1085298-.16228-.0251623-.06069-.03811269-.12576-.0381127-.19147 0-.0657.0129504-.13077.0381126-.19147.0251623-.06069.0620419-.11584.1085299-.16228l4.999995-4.999997c.06993-.0700052.15906-.117689.2561-.13701419.09704-.01932521.19764-.0094229.28905.02845329.09141.0378763.16953.1020229.22447.1843199.05493.082297.08421.179044.08414.277991v10.000017c.00007.0989-.02921.1957-.08414.278-.05494.0823-.13306.1464-.22447.1843s-.19201.0478-.28905.0284c-.09704-.0193-.18617-.067-.25609-.137z" fill="#696868" /></svg>
+                <span>Prev</span>
+            </button>
 
             <div className="page-buttons">
                 {Array.from({ length: numPages }, (v, i) => i + 1).map(page =>
@@ -140,7 +161,10 @@ function Transactions({ user }: transactionsProps) {
                 )}
             </div>
 
-            <button disabled={pageNumber === numPages ? true : false} onClick={() => setPageNumber(pageNumber + 1)}>Next</button>
+            <button disabled={pageNumber === numPages ? true : false} onClick={() => setPageNumber(pageNumber + 1)}>
+                <span>Next</span>
+                <svg fill="none" height="11" viewBox="0 0 6 11" width="6" xmlns="http://www.w3.org/2000/svg"><path d="m.853506.146465 5.000004 5.000005c.04648.04643.08336.10158.10853.16228.02516.06069.03811.12576.03811.19147 0 .0657-.01295.13077-.03811.19147-.02517.06069-.06205.11584-.10853.16228l-5.000004 5.00003c-.069927.07-.159054.1177-.256097.137-.097042.0193-.197637.0094-.289048-.0285-.091412-.0378-.16953-.102-.2244652-.1843-.0549354-.0823-.08421767-.179-.08413981-.278l-.00000043-9.999984c-.00007788-.098949.02920444-.195695.08413984-.277992.0549356-.082297.1330536-.1464431.2244646-.1843193.091412-.03787611.192007-.04777907.289049-.02845381.097042.01932521.186169.06700801.256097.13701411z" fill="#696868" /></svg>
+            </button>
         </>
     )
 }
